1bf835cf786b420a995def2a83393458d7a070e7
Sara Bensarghin <sbensarg@e1r6p10.1337.ma>
2021-12-21 17:03:19 +0100

    fix leak fds

--- microshell.c
+++ microshell.c
@@ -6,41 +6,46 @@
 /*   By: sbensarg <sbensarg@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/12 16:42:36 by sbensarg          #+#    #+#             */
-/*   Updated: 2021/12/16 02:07:41 by sbensarg         ###   ########.fr       */
+/*   Updated: 2021/12/21 16:59:21 by sbensarg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "microshell.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 
 int	fd_in;
 int	p[2];
     
-void ft_cd(char *cmd)
+void	ft_cd(char *cmd)
 {
-    int ret;
+	int	ret;
 
-    ret = chdir(cmd);
-    if (ret == -1)
-    {
-        write(2, "error: cd: cannot change directory to ", 39);
-        write(2, cmd, strlen(cmd));
-        write(2, "\n", 2);
-    }
+	ret = chdir(cmd);
+	if (ret == -1)
+	{
+		write(2, "error: cd: cannot change directory to ", 39);
+		write(2, cmd, strlen(cmd));
+		write(2, "\n", 2);
+	}
 }
 
 int main(int argc, char **argv, char **env)
 {
-    char *cmd[1024];
-    int i;
-    i = 1;
-    int j = 0;
-    int k = 0;
-    pid_t pid;
-    int flag;
-    int ret;
+    char	*cmd[1024];
+    int		i;
+    int 	j;
+    int		k;
+    pid_t	pid;
+    int		flag;
+    int		ret;
+	int fd_old;
+
 	fd_in = 0;
 	flag = 0;
+	i = 1;
+	j = 0;
+	k = 0;
     while (k < 1024)
     {
         cmd[k] = NULL;
@@ -78,6 +83,7 @@ int main(int argc, char **argv, char **env)
 					if (argv[i] != NULL)
 					{
                     	dup2(p[1], 1);
+						close(p[1]);
 					}
                     close(p[0]);
                     ret = execve(cmd[0], cmd, NULL);
@@ -90,7 +96,17 @@ int main(int argc, char **argv, char **env)
 					}
                 }
 				else
+				{
 					waitpid(pid, NULL, 0);
+					if (flag == 1)
+					{
+						close(p[1]);
+						fd_old = fd_in;
+						if (fd_old != 0)
+							close(fd_old);
+            			fd_in = p[0];
+					}
+				}
             }
             int m = 0;
             while (m < 1024)
@@ -117,7 +133,10 @@ int main(int argc, char **argv, char **env)
 					if (pid == 0)
 					{
 						if (flag == 1)
+						{
 							dup2(fd_in, 0);
+							close(fd_in);
+						}
 						ret = execve(cmd[j], cmd, env);
 						if (ret == -1)
 						{
@@ -144,11 +163,6 @@ int main(int argc, char **argv, char **env)
         else
         {
             cmd[j] = argv[i];
-			if (flag == 1)
-			{
-            	fd_in = p[0];
-				close(p[1]);
-			}
 			j++;
         }
         i++;
